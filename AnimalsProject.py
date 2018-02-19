84aa3a1e8f4bb016dd53c4f43559b377c5fd87a1
George <36277777+GeorgKutateladze@users.noreply.github.com>
2018-02-19 21:41:56 +0300

    Update AnimalsProject.py

--- AnimalsProject.py
+++ AnimalsProject.py
@@ -1,34 +1,42 @@
+#  improved with PEP8
+
 class Animal:
     def __init__(self, name):
         self.name = name
-    def get_name(self):
-        return(self.name)
 
-class Two_legs(Animal):
-    def __init__(self, name, legs = 2):
+    def get_name(self):
+        return self.name
+
+
+class TwoLegs(Animal):
+    def __init__(self, name, legs=2):
         super().__init__(name)
         self.legs = legs
+
     def bird(self):
         print(self.name, "has", self.legs, "legs.")
 
-class Four_legs(Animal):
-    def __init__(self, name, legs = 4):
+
+class FourLegs(Animal):
+    def __init__(self, name, legs=4):
         super().__init__(name)
         self.legs = legs
+
     def cattle(self):
         print(self.name, "has", self.legs, "legs.")
 
-duck = Two_legs("Duck")
-chicken = Two_legs("Chicken")
-goose = Two_legs("Goose")
+
+duck = TwoLegs("Duck")
+chicken = TwoLegs("Chicken")
+goose = TwoLegs("Goose")
 duck.bird()
 chicken.bird()
 goose.bird()
 
-cow = Four_legs("Cow")
-goat = Four_legs("Goat")
-pig = Four_legs("Pig")
-sheep = Four_legs("Sheep")
+cow = FourLegs("Cow")
+goat = FourLegs("Goat")
+pig = FourLegs("Pig")
+sheep = FourLegs("Sheep")
 cow.cattle()
 goat.cattle()
 pig.cattle()
